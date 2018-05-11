43ad887bb5b9c3794b792123459aa81df8645f99
lgm1213 <lgm1213@gmail.com>
2018-05-11 16:59:04 -0400

    JSON response from Google API is formated and sent to console

--- app.js
+++ app.js
@@ -4,5 +4,8 @@ request({
 	url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1600%20Amphitheatre%20Parkway%20Mountain%20View',
 	json: true
 }, (error, response, body) => {
-  console.log(body);
+	console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Longitude: ${body.results[0].geometry.location.lat}`);
+  console.log(`Latitude: ${body.results[0].geometry.location.lng}`);
+
 });
