11f71f6e642987813023114864f730faf0a57007
lgm1213 <lgm1213@gmail.com>
2018-05-16 13:16:30 -0400

    refactored using callbacks and moved logic to a dedicated file

---
+++ geocode/geocode.js
@@ -0,0 +1,24 @@
+const request = require('request');
+
+var geocodeAddress = (address, callback) => {
+	var encodedAddress = encodeURIComponent(address);
+
+	request({
+		url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+		json: true
+	}, (error, response, body) => {
+		if (error) {
+			callback('Error Occured, unable to connect to Google Server');
+		} else if (body.status === 'ZERO_RESULTS') {
+	    callback('Unable to locate address entered.');
+		} else if (body.status === 'OK') {
+			callback(undefined, {
+				address: body.results[0].formatted_address,
+		    longitude: body.results[0].geometry.location.lat,
+		    latitude: body.results[0].geometry.location.lng
+			});
+		}
+	});
+};
+
+module.exports.geocodeAddress = geocodeAddress;
